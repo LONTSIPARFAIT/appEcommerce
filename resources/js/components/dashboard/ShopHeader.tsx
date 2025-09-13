ddc6cfc90ef70d06613d0aacfe6008a9e19952a3
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-13 14:06:40 +0100

    first commit

--- resources/js/components/dashboard/ShopHeader.tsx
+++ resources/js/components/dashboard/ShopHeader.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function ShopHeader() {
   return (
-    <div>
-      
+    <div className='flex gap-2'>
+      <a href="/">Home</a>
+      <a href="/login">Login</a>
     </div>
   )
 }
