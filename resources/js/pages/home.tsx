bc818cc28dfa8757136dc403c72a247ef30336fe
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-13 16:05:50 +0100

    first commit

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -3,6 +3,7 @@ import ShopFrontLayout from '@/layouts/shop-front-layout'
 export default function home() {
   return (
     <ShopFrontLayout>
+        <div className="min-h-screen"></div>
       <h2>Bienvenue sue mon site de E-commerce</h2>
     </ShopFrontLayout>
   )
