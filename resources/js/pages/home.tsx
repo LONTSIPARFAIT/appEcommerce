71a4ba17fc6d3ccf45ba7b8f98741e424154ff3c
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-13 14:18:53 +0100

    first commit

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,9 +1,10 @@
+import ShopFrontLayout from '@/layouts/shop-front-layout'
 import React from 'react'
 
 export default function home() {
   return (
-    <div>
+    <ShopFrontLayout>
       <h2>Bienvenue sue mon site de E-commerce</h2>
-    </div>
+    </ShopFrontLayout>
   )
 }
