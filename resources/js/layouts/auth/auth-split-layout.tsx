55824663be5cd05804a0f6f93eae4533c9b6ff14
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-14 00:48:38 +0100

    first commit

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -14,6 +14,7 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
     // const { name, quote } = usePage<SharedData>().props;
 
     return (
+        <GlobalLayout>
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <AuthBackground />
             {/* <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
