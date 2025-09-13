8076bf81da8e18723fba70201c15cc19b28262c3
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-13 15:34:45 +0100

    first commit

--- resources/js/layouts/shop-front-layout.tsx
+++ resources/js/layouts/shop-front-layout.tsx
@@ -1,14 +1,22 @@
 
 import ShopFooter from '@/components/dashboard/ShopFooter'
 import ShopHeader from '@/components/dashboard/ShopHeader'
+import { ThemeProvider } from '@/components/theme-provider'
 import React, { ReactNode } from 'react'
 
 export default function ShopFrontLayout({children}:{children:ReactNode}) {
   return (
     <div>
-        <ShopHeader />
-            {children}
-        <ShopFooter />
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ShopHeader />
+                {children}
+            <ShopFooter />
+        </ThemeProvider>
     </div>
   )
 }
