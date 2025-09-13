e458b16b97b99629d316d2c49fc98203351ff61c
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-13 14:09:32 +0100

    first commit

--- resources/js/components/dashboard/ShopFooter.tsx
+++ resources/js/components/dashboard/ShopFooter.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function ShopFooter() {
   return (
     <footer className=''>
-      
+        <a href="/">Home</a>
+        <a href="/login">Login</a>
     </footer>
   )
 }
