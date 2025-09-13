b6cb158cf15c2f85ba9ce36af0ed387461b7d80c
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-13 14:17:17 +0100

    first commit

--- resources/js/layouts/shop-front-layout.tsx
+++ resources/js/layouts/shop-front-layout.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import ShopFooter from '@/components/dashboard/ShopFooter'
+import ShopHeader from '@/components/dashboard/ShopHeader'
+import React, { ReactNode } from 'react'
 
-export default function ShopFrontLayout() {
+export default function ShopFrontLayout({children}:{children:ReactNode}) {
   return (
     <div>
-      
+        <ShopHeader />
+            {children}
+        <ShopFooter />
     </div>
   )
 }
