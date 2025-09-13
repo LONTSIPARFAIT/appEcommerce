f9a6f71c36dc9033ecb9b1ec5659873b65e6d4a9
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-14 00:46:14 +0100

    first commit

--- resources/js/layouts/global-layout.tsx
+++ resources/js/layouts/global-layout.tsx
@@ -6,8 +6,13 @@ import React, { ReactNode } from 'react'
 
 export default function GlobalLayout({children}:{children:ReactNode}) {
   return (
-    <div>
-       
-    </div>
+     <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+        {children}
+    </ThemeProvider>
   )
 }
