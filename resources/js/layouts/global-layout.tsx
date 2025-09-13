d39d8169dd9033bb1faa059063a99e9f1f27d755
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-14 00:44:19 +0100

    first commit

---
+++ resources/js/layouts/global-layout.tsx
@@ -0,0 +1,13 @@
+
+import ShopFooter from '@/components/frontend/ShopFooter'
+import ShopHeader from '@/components/frontend/ShopHeader'
+import { ThemeProvider } from '@/components/theme-provider'
+import React, { ReactNode } from 'react'
+
+export default function GlobalLayout({children}:{children:ReactNode}) {
+  return (
+    <div>
+       
+    </div>
+  )
+}
