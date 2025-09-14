48360c9b35e7bbad6792cfc272e4f5d077ca62ba
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-14 21:07:04 +0100

    first commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = [ 'name', 'slug', 'price', 'original_price', 'rating', 'reviewCount', 'description', 'features', 'image', 'images', 'colors', 'sizes', 'is_featured', 'in_stock' ];
+
+    protected $casts =[
+        'in_stock'=>'boolean',
+        'is_featured'=>'boolean',
+    ];
 }
