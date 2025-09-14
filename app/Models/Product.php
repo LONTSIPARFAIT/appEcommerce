1b973d44806306af61800dc84f8110d458dc6270
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-14 21:04:55 +0100

    first commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,20 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = [
-        'name',
-'       slug'
-price
-original_price
-rating
-reviewCount
-description
-features
-image
-images
-colors
-sizes
-is_featured
-in_stock
-    ]
+    protected $fillable = [ 'name', 'slug', 'price', 'original_price', 'rating', 'reviewCount', 'description', 'features', 'image', 'images', 'colors', 'sizes', 'is_featured', 'in_stock' ];
 }
--- database/migrations/2025_09_14_193349_create_products_table.php
+++ database/migrations/2025_09_14_193349_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignIdFor('category_id');
             $table->string('slug')->unique();
             $table->decimal('price',10,2)->default(0);
             $table->decimal('original_price',10,2)->default(0);
