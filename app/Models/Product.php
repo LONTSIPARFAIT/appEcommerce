459dc778723f74d064a94133308cec3ec5e4c6b4
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-14 21:27:14 +0100

    first commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -23,4 +23,8 @@ class Product extends Model
     public function category(){
         return $this->belongsTo(Category::class);
     }
+
+    public function ScopeSimilar($query, $productId){
+        $product = static::firstOrFail();
+    }
 }
