d101b5fe7cab83caa42fcac7ae53e7cfa2bda432
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-14 14:25:50 +0100

    first commit

--- resources/js/pages/dashboard/index.tsx
+++ resources/js/pages/dashboard/index.tsx
@@ -1,10 +1,11 @@
+import AppLayout from '@/layouts/app-layout'
 import React from 'react'
 
 export default function index() {
   return (
-    <div>
-
-    </div>
+    <AppLayout breadcrumbs={breadcrumbs}>
+        k
+    </AppLayout>
   )
 }
 
