224e391c38e452f925b46232d5d6f71c2c32421d
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-14 20:02:41 +0100

    first commit

--- app/Http/Controllers/Shop/HomeController.php
+++ app/Http/Controllers/Shop/HomeController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Shop;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function get_home_data(){
+        $categories = Category::latest()->get();
+
+        Inertia::render('home', [
+            'cate'
+        ]);
+    }
 }
