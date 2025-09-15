2236152fc7339adc0a2095b122dfd07a482f9288
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-15 12:01:34 +0100

    first commit

--- resources/js/components/dashboard/ProductsDataTable.tsx
+++ resources/js/components/dashboard/ProductsDataTable.tsx
@@ -490,6 +490,20 @@ export default function DashboardDataTable() {
                     />
                     <InputError message={errors.features} className="mt-2" />
                   </div>
+                  <div className="space-y-2">
+                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                        <FormControl>
+                        <SelectTrigger>
+                            <SelectValue placeholder="Select a verified email to display" />
+                        </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                        <SelectItem value="m@example.com">m@example.com</SelectItem>
+                        <SelectItem value="m@google.com">m@google.com</SelectItem>
+                        <SelectItem value="m@support.com">m@support.com</SelectItem>
+                        </SelectContent>
+                    </Select>
+                  </div>
                   </div>
                   <div className="grid w-full gap-3">
                     <Label htmlFor="message">Description du Produit</Label>
