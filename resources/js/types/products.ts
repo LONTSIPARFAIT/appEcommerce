cf837978f0bde9fdffec7f165127614757731cf3
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-15 10:41:48 +0100

    first commit

--- resources/js/types/products.ts
+++ resources/js/types/products.ts
@@ -6,11 +6,13 @@ export interface SimilarProduct {
   reviewCount: number;
   image: string;
 }
-export interface SimilarProduct {
-  id: string;
+export interface CreateProductItem {
+  name: string;
   name: string;
   price: number;
   rating: number;
   reviewCount: number;
   image: string;
 }
+
+// '', 'slug', 'price', 'original_price', 'rating', 'reviewCount', 'description', 'features', 'image', 'images', 'colors', 'sizes', 'is_featured', 'in_stock'
