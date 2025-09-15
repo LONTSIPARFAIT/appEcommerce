b8ff862ff330689cd963b602d311101169d767fb
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-15 22:23:59 +0100

    first commit

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ProductController extends Controller
 {
     public function list_categories(){
-        $categories = Category::latest()->get();
+        $categories = Product::latest()->get();
 
         return Inertia::render('dashboard/categories/index', [
             'categories' => $categories
