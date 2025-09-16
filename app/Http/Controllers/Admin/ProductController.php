d3add33f8437a6096112aace8cc1e442e106cec3
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-16 07:23:12 +0100

    first commit

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -19,13 +19,34 @@ class ProductController extends Controller
             'categories' => $categories,
         ]);
     }
-    public function save_product(){
-        $products = Product::latest()->get();
-        $categories = Category::latest()->get();
-
-        return Inertia::render('dashboard/products/index', [
-            'products' => $products,
-            'categories' => $categories,
+    public function save_category(Request $request){
+        $request->validate([
+            'name' => 'string|required|max:255',
+            'color' => 'string|required',
+            'description' => 'string|nullable',
+            'image' => 'required|nullable|max:2048',
         ]);
+
+        //slug
+        $slug = Str::slug($request->name);
+        $image = '';
+
+        //images
+        if($request->hasFile('image')){
+            $image = $request->file('image')->store('categories','public');
+        }
+
+        $new_category = [
+            'name' => $request->name,
+            'slug' => $slug,
+            'image' => $image,
+            'color' => $request->color,
+        ];
+
+        $cat = Category::create($new_category);
+
+        // dd($cat);
+
+        return to_route('dashboard.categories.index');
     }
 }
